bf425cc04125e30d6d67c34aeb3ba9337dcc0421
Pedro Assunção <assuncas@gmail.com>
2024-01-04 10:33:50 +0000

    Putting back pipes

--- lib/styler.ex
+++ lib/styler.ex
@@ -19,7 +19,7 @@ defmodule Styler do
 
   @styles [
     Styler.Style.ModuleDirectives,
-    # Styler.Style.Pipes,
+    Styler.Style.Pipes,
     Styler.Style.SingleNode,
     # Styler.Style.Defs,
     Styler.Style.Blocks
